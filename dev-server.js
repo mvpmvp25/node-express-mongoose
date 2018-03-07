60fc3dfbc8b8606299aaded1b8e922fb5bb8bb15
fizz <441980627@qq.com>
2018-03-07 11:13:05 +0800

    改了下ip

--- dev-server.js
+++ dev-server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true })); // 获取post请求参数需
 
 
 app.all('*', function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://10.101.90.59:8020');
+    res.header('Access-Control-Allow-Origin', 'http://10.11.1.78:8016');
 	res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
@@ -19,7 +19,7 @@ app.all('*', function(req, res, next) {
 
 Router(app);
  
-const server = app.listen(8037, "10.101.90.59", function () { // 本机IP
+const server = app.listen(8037, "10.11.1.78", function () { // 本机IP
   let host = server.address().address
   let port = server.address().port
   console.log("http://" + host + ":" + port + " ready");
